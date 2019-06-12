eed97259a76859749b65c6423825fd4b3581ee20
Andrey <ushakov.andrey.34@gmail.com>
2019-06-12 22:29:45 +0200

    Update example : added row.

--- example/example.dart
+++ example/example.dart
@@ -3,19 +3,27 @@ import 'package:esc_pos_printer/src/printer.dart';
 
 void main() {
   Printer.connect('192.168.0.123').then((printer) {
-    printer.reset();
     printer.println(PosString('Normal text'));
     printer.println(PosString('Bold text', bold: true));
     printer.println(PosString('Reverse text', reverse: true));
-    printer.println(PosString('Underlined text', underline: true));
+    printer.println(PosString('Underlined text', underline: true),
+        linesAfter: 1);
     printer.println(PosString('Align left', align: PosTextAlign.left));
     printer.println(PosString('Align center', align: PosTextAlign.center));
-    printer.println(PosString('Align right', align: PosTextAlign.right));
+    printer.println(PosString('Align right', align: PosTextAlign.right),
+        linesAfter: 1);
+    printer.printRow(
+      [3, 6, 3],
+      [
+        PosString('col3', align: PosTextAlign.center, underline: true),
+        PosString('col6', align: PosTextAlign.center, underline: true),
+        PosString('col3', align: PosTextAlign.center, underline: true)
+      ],
+    );
     printer.println(PosString('Text size 200%',
         height: PosTextSize.size2, width: PosTextSize.size2));
 
     printer.cut();
-
     printer.disconnect();
   });
 }
