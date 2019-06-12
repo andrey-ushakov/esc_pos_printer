32edba1dbc6e965d0b71833fe6cbac5324a2a701
Andrey <ushakov.andrey.34@gmail.com>
2019-06-12 12:17:05 +0200

    Added main commands

---
+++ lib/src/commands.dart
@@ -0,0 +1,29 @@
+const esc = '\x1B';
+const gs = '\x1D';
+
+// Miscellaneous
+const cInit = '$esc@'; // Initialize printer
+
+// Mech. Control
+const cCut = '${gs}V0'; // Cut paper
+const cCutFull = '${esc}i'; // Execute paper full cut
+const cCutPart = '${esc}m'; // Execute paper partial cut.
+
+// Character
+const cReverseOn = '${gs}B1'; // Turn white/black reverse print mode on
+const cReverseOff = '${gs}B0'; // Turn white/black reverse print mode off
+const cSizeN = '$gs!'; // Select character size [N]
+const cUnderlineOff = '$esc-0'; // Turns off underline mode
+const cUnderline1dot = '$esc-1'; // Turns on underline mode (1-dot thick)
+const cUnderline2dots = '$esc-2'; // Turns on underline mode (2-dots thick)
+const cBoldOn = '${esc}E1'; // Turn emphasized mode on
+const cBoldOff = '${esc}E0'; // Turn emphasized mode off
+
+// Print Position
+const cAlignLeft = '${esc}a0'; // Left justification
+const cAlignCenter = '${esc}a1'; // Centered
+const cAlignRight = '${esc}a2'; // Right justification
+
+// Print
+const cFeedN = '${esc}d'; // Print and feed n lines [N]
+const cReverseFeedN = '${esc}e'; // Print and reverse feed n lines [N]
