883a2aacde287d4c9e2308f65ce84f4722c5caaa
Andrey <ushakov.andrey.34@gmail.com>
2020-01-15 16:12:50 +0100

    Example bluetooth project: can scan and print once.

--- example/blue/lib/main.dart
+++ example/blue/lib/main.dart
@@ -1,5 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_blue/flutter_blue.dart';
+import 'package:flutter_bluetooth_basic/flutter_bluetooth_basic.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Bluetooth demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -19,82 +20,60 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
-  final FlutterBlue flutterBlue = FlutterBlue.instance;
+  // final FlutterBlue flutterBlue = FlutterBlue.instance;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<ScanResult> _scanResults = [];
+  BluetoothManager bluetoothManager = BluetoothManager.instance;
+  bool _connected = false;
 
-  void _scanDevices() {
-    setState(() {
-      _scanResults = [];
-    });
-
-    // Start scanning
-    widget.flutterBlue.startScan(timeout: Duration(seconds: 4));
-
-    // Listen to scan results
-    var subscription = widget.flutterBlue.scanResults.listen((scanResult) {
-      // do something with scan result
-      // device = scanResult.device;
-      // print('${device.name} found! rssi: ${scanResult.rssi}');
-
-      print('FOUND !!! ${scanResult.length}');
-      scanResult.forEach((ScanResult scanRes) async {
-        if (scanRes.device.name.isNotEmpty) {
-          if (!_scanResults.contains(scanRes)) {
-            _scanResults.add(scanRes);
-            setState(() {
-              _scanResults
-                  .sort((scanRes1, scanRes2) => scanRes2.rssi - scanRes1.rssi);
-            });
-          }
-          print(
-              '\t> ${scanRes.device.name} : ${scanRes.rssi} : ${_scanResults.length}');
-        }
-      });
-    });
-
-    // Stop scanning
-    widget.flutterBlue.stopScan();
+  Future sleep3() {
+    return Future<dynamic>.delayed(const Duration(seconds: 7), () => "1");
   }
 
-  void testPrint(ScanResult scanResult, BuildContext ctx) async {
-    BluetoothDevice device = scanResult.device;
-    print('Sending test print to... ${device.name}');
+  void _testPrint(BluetoothDevice printer) async {
+    // print('Test print.... name: ${printer.name}');
+    if (printer != null && printer.address != null) {
+      // Connect
+      await bluetoothManager.connect(printer);
 
-    await device.connect();
-    print('\t>connected');
+      // Subscribe to the events
+      bluetoothManager.state.listen((state) async {
+        // print('**************cur device status: $state');
+        switch (state) {
+          case BluetoothManager.CONNECTED:
+            print('********************* CONNECTED');
+            // to avoid double call
+            if (!_connected) {
+              print('@@@@SEND DATA......');
+              final List<int> bytes = latin1.encode('test!\n\n\n').toList();
+              await bluetoothManager.writeData(bytes);
+              // print('################## print send #############');
+              // bluetoothManager.
+            }
+            sleep3().then((dynamic printer) async {
+              print('@@@@DISCONNECTING......');
+              await bluetoothManager.disconnect();
+            });
 
-    List<BluetoothService> services = await device.discoverServices();
-    services.forEach((service) async {
-      print('> Service // isPrimary: ${service.isPrimary} // ${service.uuid}');
+            _connected = true;
+            break;
+          case BluetoothManager.DISCONNECTED:
+            print('********************* DISCONNECTED');
+            _connected = false;
+            break;
+          default:
+            break;
+        }
+      });
 
-      // Reads all characteristics
-      final characteristics = service.characteristics;
-      for (BluetoothCharacteristic c in characteristics) {
-        // List<int> value = await c.read();
-        // print('\t\t## charact. // $value');
-      }
-    });
-
-    // const esc = '\x1B';
-    // const cInit = '$esc@'; // Initialize printer
-
-    // reset
-    // _socket.write(cInit);
-    // print text
-    // _socket.add(latin1.encode(text));
-    // empty lines
-    // _socket.write(List.filled(5, '\n').join());
-    // reset
-    // _socket.write(cInit);
-
-    device.disconnect();
-    print('\t>disconnected');
+      // TODO show message "Data sent"
+    } else {
+      // TODO show message "Can't connect to the device"
+    }
   }
 
   @override
@@ -103,49 +82,64 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: ListView.builder(
-        itemCount: _scanResults.length,
-        itemBuilder: (BuildContext context, int index) {
-          return InkWell(
-            onTap: () => testPrint(_scanResults[index], context),
-            child: Column(
-              children: <Widget>[
-                Container(
-                  height: 60,
-                  padding: EdgeInsets.only(left: 10),
-                  alignment: Alignment.centerLeft,
-                  child: Row(
-                    children: <Widget>[
-                      Icon(Icons.print),
-                      SizedBox(width: 10),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Text(
-                              'Signal ${_scanResults[index].rssi + 100}% : ${_scanResults[index].device.name}',
-                            ),
-                            Text(
-                              'Click to print a test receipt',
-                              style: TextStyle(color: Colors.grey[700]),
-                            ),
-                          ],
+      body: StreamBuilder<List<BluetoothDevice>>(
+        stream: bluetoothManager.scanResults,
+        initialData: [],
+        builder: (c, snapshot) => Column(
+          children: snapshot.data
+              .map((d) => InkWell(
+                    onTap: () => _testPrint(d),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          height: 60,
+                          padding: EdgeInsets.only(left: 10),
+                          alignment: Alignment.centerLeft,
+                          child: Row(
+                            children: <Widget>[
+                              Icon(Icons.print),
+                              SizedBox(width: 10),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Text(d.name ?? ''),
+                                    Text(d.address),
+                                    Text(
+                                      'Click to print a test receipt',
+                                      style: TextStyle(color: Colors.grey[700]),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
                         ),
-                      )
-                    ],
-                  ),
-                ),
-                Divider(),
-              ],
-            ),
-          );
-        },
+                        Divider(),
+                      ],
+                    ),
+                  ))
+              .toList(),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _scanDevices,
-        tooltip: 'Scan',
-        child: Icon(Icons.add),
+      floatingActionButton: StreamBuilder<bool>(
+        stream: bluetoothManager.isScanning,
+        initialData: false,
+        builder: (c, snapshot) {
+          if (snapshot.data) {
+            return FloatingActionButton(
+              child: Icon(Icons.stop),
+              onPressed: () => bluetoothManager.stopScan(),
+              backgroundColor: Colors.red,
+            );
+          } else {
+            return FloatingActionButton(
+                child: Icon(Icons.search),
+                onPressed: () =>
+                    bluetoothManager.startScan(timeout: Duration(seconds: 4)));
+          }
+        },
       ),
     );
   }
