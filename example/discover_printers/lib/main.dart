1e426dcd7b73d0168ebb97259ecb50b9432a6e60
Andrey <ushakov.andrey.34@gmail.com>
2020-03-16 14:26:04 +0100

    Added demo receipt example

--- example/discover_printers/lib/main.dart
+++ example/discover_printers/lib/main.dart
@@ -1,5 +1,8 @@
 import 'dart:io';
 import 'dart:typed_data';
+import 'package:intl/intl.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart' hide Image;
 import 'package:esc_pos_printer/esc_pos_printer.dart';
 import 'package:flutter/services.dart';
@@ -89,7 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
       });
   }
 
-  Future<Ticket> demoTicket(PaperSize paper) async {
+  Future<Ticket> testTicket(PaperSize paper) async {
     final Ticket ticket = Ticket(paper);
 
     ticket.text(
@@ -158,116 +161,145 @@ class _MyHomePageState extends State<MyHomePage> {
     return ticket;
   }
 
-  Ticket testPrinter1(PaperSize paper) {
-    final Ticket ticket = Ticket(paper);
+  Future<Ticket> demoReceipt80mm() async {
+    final Ticket ticket = Ticket(PaperSize.mm80);
 
-    ticket.text('Test 1. Text Styles, Code Tables & Positions',
-        styles: PosStyles(align: PosAlign.center), linesAfter: 2);
+    // Print image
+    final ByteData data = await rootBundle.load('assets/rabbit_black.jpg');
+    final Uint8List bytes = data.buffer.asUint8List();
+    final Image image = decodeImage(bytes);
+    ticket.image(image);
 
-    ticket.text('Regular: Hello world!');
-    ticket.text('westEur: àÀ èÈ éÉ ûÛ üÜ çÇ ôÔ blåbærgrød',
-        styles: PosStyles(codeTable: PosCodeTable.westEur));
-    ticket.text('Bold text', styles: PosStyles(bold: true));
-    ticket.text('Reverse text', styles: PosStyles(reverse: true));
-    ticket.text('Underlined text',
-        styles: PosStyles(underline: true), linesAfter: 1);
-    ticket.text('Align left', styles: PosStyles(align: PosAlign.left));
-    ticket.text('Align center', styles: PosStyles(align: PosAlign.center));
-    ticket.text('Align right',
-        styles: PosStyles(align: PosAlign.right), linesAfter: 1);
+    ticket.text('GROCERYLY',
+        styles: PosStyles(
+          align: PosAlign.center,
+          height: PosTextSize.size2,
+          width: PosTextSize.size2,
+        ),
+        linesAfter: 1);
+
+    ticket.text('889  Watson Lane', styles: PosStyles(align: PosAlign.center));
+    ticket.text('New Braunfels, TX', styles: PosStyles(align: PosAlign.center));
+    ticket.text('Tel: 830-221-1234', styles: PosStyles(align: PosAlign.center));
+    ticket.text('Web: www.example.com',
+        styles: PosStyles(align: PosAlign.center), linesAfter: 1);
+
+    ticket.text('------------------------------------------------');
+    ticket.row([
+      PosColumn(text: 'Qty', width: 1),
+      PosColumn(text: 'Item', width: 7),
+      PosColumn(
+          text: 'Price', width: 2, styles: PosStyles(align: PosAlign.right)),
+      PosColumn(
+          text: 'Total', width: 2, styles: PosStyles(align: PosAlign.right)),
+    ]);
+
+    ticket.row([
+      PosColumn(text: '2', width: 1),
+      PosColumn(text: 'ONION RINGS', width: 7),
+      PosColumn(
+          text: '0.99', width: 2, styles: PosStyles(align: PosAlign.right)),
+      PosColumn(
+          text: '1.98', width: 2, styles: PosStyles(align: PosAlign.right)),
+    ]);
+    ticket.row([
+      PosColumn(text: '1', width: 1),
+      PosColumn(text: 'PIZZA', width: 7),
+      PosColumn(
+          text: '3.45', width: 2, styles: PosStyles(align: PosAlign.right)),
+      PosColumn(
+          text: '3.45', width: 2, styles: PosStyles(align: PosAlign.right)),
+    ]);
+    ticket.row([
+      PosColumn(text: '1', width: 1),
+      PosColumn(text: 'SPRING ROLLS', width: 7),
+      PosColumn(
+          text: '2.99', width: 2, styles: PosStyles(align: PosAlign.right)),
+      PosColumn(
+          text: '2.99', width: 2, styles: PosStyles(align: PosAlign.right)),
+    ]);
+    ticket.row([
+      PosColumn(text: '3', width: 1),
+      PosColumn(text: 'CRUNCHY STICKS', width: 7),
+      PosColumn(
+          text: '0.85', width: 2, styles: PosStyles(align: PosAlign.right)),
+      PosColumn(
+          text: '2.55', width: 2, styles: PosStyles(align: PosAlign.right)),
+    ]);
+    ticket.text('------------------------------------------------');
 
     ticket.row([
       PosColumn(
-        text: 'col3',
-        width: 3,
-        styles: PosStyles(align: PosAlign.center, underline: true),
-      ),
+          text: 'TOTAL',
+          width: 6,
+          styles: PosStyles(
+            height: PosTextSize.size2,
+            width: PosTextSize.size2,
+          )),
       PosColumn(
-        text: 'col6',
-        width: 6,
-        styles: PosStyles(align: PosAlign.center, underline: true),
-      ),
+          text: '\$10.97',
+          width: 6,
+          styles: PosStyles(
+            align: PosAlign.right,
+            height: PosTextSize.size2,
+            width: PosTextSize.size2,
+          )),
+    ]);
+    ticket.text('------------------------------------------------',
+        linesAfter: 1);
+
+    ticket.row([
       PosColumn(
-        text: 'col3',
-        width: 3,
-        styles: PosStyles(align: PosAlign.center, underline: true),
-      ),
+          text: 'Cash',
+          width: 8,
+          styles: PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
+      PosColumn(
+          text: '\$15.00',
+          width: 4,
+          styles: PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
+    ]);
+    ticket.row([
+      PosColumn(
+          text: 'Change',
+          width: 8,
+          styles: PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
+      PosColumn(
+          text: '\$4.03',
+          width: 4,
+          styles: PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
     ]);
 
-    ticket.text('Text size 200%',
-        styles: PosStyles(
-          height: PosTextSize.size2,
-          width: PosTextSize.size2,
-          align: PosAlign.center,
-        ));
+    ticket.feed(2);
+    ticket.text('Thank you!',
+        styles: PosStyles(align: PosAlign.center, bold: true));
 
-    ticket.feed(3);
-    ticket.cut();
-    return ticket;
-  }
-
-  Future<Ticket> testPrinter2_1(PaperSize paper) async {
-    final Ticket ticket = Ticket(paper);
-
-    ticket.text('Test 2. Images',
+    final now = DateTime.now();
+    final formatter = DateFormat('MM/dd/yyyy H:m');
+    final String timestamp = formatter.format(now);
+    ticket.text(timestamp,
         styles: PosStyles(align: PosAlign.center), linesAfter: 2);
 
-    // Print image
-    final ByteData data = await rootBundle.load('assets/logo.png');
-    final Uint8List bytes = data.buffer.asUint8List();
-    final Image image = decodeImage(bytes);
+    try {
+      const String qrData = 'example.com';
+      const double qrSize = 200;
+      final uiImg = await QrPainter(
+        data: qrData,
+        version: QrVersions.auto,
+        gapless: false,
+      ).toImageData(qrSize);
+      final dir = await getTemporaryDirectory();
+      final pathName = '${dir.path}/qr_tmp.png';
+      final qrFile = File(pathName);
+      final imgFile = await qrFile.writeAsBytes(uiImg.buffer.asUint8List());
+      final img = decodeImage(imgFile.readAsBytesSync());
 
-    ticket.text('Command: ESC *');
-    ticket.image(image);
-
-    ticket.feed(2);
-    return ticket;
-  }
-
-  Future<Ticket> testPrinter2_2(PaperSize paper) async {
-    final Ticket ticket = Ticket(paper);
-
-    // Print image
-    final ByteData data = await rootBundle.load('assets/logo.png');
-    final Uint8List bytes = data.buffer.asUint8List();
-    final Image image = decodeImage(bytes);
-
-    ticket.text('Command: GS v 0');
-    ticket.imageRaster(image, imageFn: PosImageFn.bitImageRaster);
+      ticket.image(img);
+    } catch (e) {
+      print(e);
+    }
 
     ticket.feed(2);
-    return ticket;
-  }
-
-  Future<Ticket> testPrinter2_3(PaperSize paper) async {
-    final Ticket ticket = Ticket(paper);
-
-    // Print image
-    final ByteData data = await rootBundle.load('assets/logo.png');
-    final Uint8List bytes = data.buffer.asUint8List();
-    final Image image = decodeImage(bytes);
-
-    ticket.text('Command: GS ( L');
-    ticket.imageRaster(image, imageFn: PosImageFn.graphics);
-
-    ticket.feed(3);
     ticket.cut();
-    return ticket;
-  }
-
-  Ticket testPrinter3(PaperSize paper) {
-    final Ticket ticket = Ticket(paper);
-
-    ticket.text('Test 3. Barcode UPC-A',
-        styles: PosStyles(align: PosAlign.center), linesAfter: 2);
-
-    // Print barcode
-    final List<int> barData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 4];
-    ticket.barcode(Barcode.upcA(barData));
-
-    ticket.feed(3);
-    ticket.cut();
-
     return ticket;
   }
 
@@ -275,38 +307,14 @@ class _MyHomePageState extends State<MyHomePage> {
     final PrinterNetworkManager printerManager = PrinterNetworkManager();
     printerManager.selectPrinter(printerIp, port: 9100);
 
-    // TODO Don't forget to choose printer's paper
-    const PaperSize paper = PaperSize.mm80;
-
-    // Demo ticket
-    final PosPrintResult res =
-        await printerManager.printTicket(await demoTicket(paper));
-
-    // TODO Comment the above printTicket line and uncomment those to run a more complete test.
-    // Don't forget to add your results here (WIFI/Network): https://github.com/andrey-ushakov/esc_pos_printer/blob/master/printers.md
-    // or here (Bluetooth): https://github.com/andrey-ushakov/esc_pos_bluetooth/blob/master/printers.md
-    // NOTE: If your printer can't handle all tests together, you can run them individually
-
-    // TEST PRINTER - START
-    // Test1 - Texts
-    // await printerManager.printTicket(testPrinter1(paper));
-    // sleep(Duration(seconds: 3));
-
-    // Test2 - Images (3 commands)
-    // await printerManager.printTicket(await testPrinter2_1(paper));
-    // sleep(Duration(seconds: 3));
-
-    // await printerManager.printTicket(await testPrinter2_2(paper));
-    // sleep(Duration(seconds: 3));
-
-    // await printerManager.printTicket(await testPrinter2_3(paper));
-    // sleep(Duration(seconds: 3));
-
-    // Test3 - Barcode
+    // TEST PRINT
+    // const PaperSize paper = PaperSize.mm80; // TODO Don't forget to choose printer's paper
     // final PosPrintResult res =
-    //     await printerManager.printTicket(testPrinter3(paper));
-    // sleep(Duration(seconds: 3));
-    // TEST PRINTER - END
+    //     await printerManager.printTicket(await testTicket(paper));
+
+    // DEMMO RECEIPT
+    final PosPrintResult res =
+        await printerManager.printTicket(await demoReceipt80mm());
 
     final snackBar =
         SnackBar(content: Text(res.msg, textAlign: TextAlign.center));
